9902d440d69dbf61cc7a56545b788a7c393419c1
asaudu <adaoraa.91@gmail.com>
2024-06-12 20:17:24 -0500

    file data worked out

--- main.go
+++ main.go
@@ -1,22 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"addyCodes.com/price-calculator/prices"
+)
 
 func main() {
-	prices := []float64{10, 20, 30, 40}
+	//prices := []float64{10, 20, 30, 40}
 	var taxRates []float64 = []float64{0, 0.07, 0.1, 0.15}
 
-	var result map[float64][]float64 = make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxIncludedPries := make([]float64, len(prices))
-
-		for priceIndex, price := range prices {
-			taxIncludedPries[priceIndex] = price * (1 + taxRate)
-		}
-
-		result[taxRate] = taxIncludedPries
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
-	fmt.Println("Here's the result", result)
 }
