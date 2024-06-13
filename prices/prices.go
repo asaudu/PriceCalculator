8d35003bb7cc3c52a858ddb860836687e5b92212
asaudu <adaoraa.91@gmail.com>
2024-06-12 20:34:30 -0500

    created a new package for converting strings to floats

--- prices/prices.go
+++ prices/prices.go
@@ -4,7 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+
+	"addyCodes.com/price-calculator/prices/conversion"
 )
 
 type TaxIncludedPriceJob struct {
@@ -40,21 +41,16 @@ func (job *TaxIncludedPriceJob) LoadData() {
 		return
 	}
 
-	prices := make([]float64, len(lines))
+	prices, err := conversion.StringsToFloats(lines)
 
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-
-		if err != nil {
-			fmt.Println("An error occurred when converting prices to floats")
-			file.Close()
-			return
-		}
-
-		prices[lineIndex] = floatPrice
+	if err != nil {
+		fmt.Println("An error occurred when converting prices to floats")
+		file.Close()
+		return
 	}
 
 	job.InputPrices = prices
+	file.Close()
 }
 
 func (job *TaxIncludedPriceJob) Process() {
